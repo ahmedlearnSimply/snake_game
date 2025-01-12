354bb33bb583105b435fb62aa44cfe0325193c51
ahmed adel <ahmedadel123422@gmail.com>
2025-01-12 17:50:23 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_cloud_firestore/firebase_cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:snake_game/screens/home_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FirebaseFirestore.instance.collection('highScores');
+
   runApp(const MainApp());
 }
 
